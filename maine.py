4a4e853539815c5ce48e97cb73e4e30dc8c216b2
cheegiiiii <chinguun273@gmail.com>
2026-03-18 10:48:55 +0800

    rock paper scissors

--- maine.py
+++ maine.py
@@ -4,3 +4,37 @@ with open("data.txt", "r") as file:
     print(file.read())
 
 print("kebab :3")
+
+import random
+
+list = ["rock", "paper", "scissors"]
+a = input("Rock, Paper, Scissors!: ")
+b = random.list
+user_score = 0
+computer_score = 0 
+ 
+while user_score < 3 and computer_score < 0:
+    if a == b:
+        print("Tie!")
+    elif a == "rock" and b == "paper":
+        computer_score += 1
+        print("computer wins")
+    elif a == "scissors" and b == "paper":
+        user_score += 1
+        print("you win")
+    elif a == "rock" and b == "scissors":
+        user_score += 1
+        print("you win")
+    elif a == "scissors" and b == "rock":
+        computer_score += 1
+        print("computer wins")
+    elif a == "paper" and b == "scissors":
+        computer_score += 1
+        print("computer wins")
+    elif a == "paper" and b == "rock":
+        user_score += 1
+        print("you win")
+    else: 
+        print("There's something wrong")
+        
+
