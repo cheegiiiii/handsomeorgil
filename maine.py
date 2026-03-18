ee239f895e5fc4187c3e6c8a25e82537012f14a7
Batorgil09100 <Batorgil09100@gmail.com>
2026-03-18 10:59:02 +0800

    idk

--- maine.py
+++ maine.py
@@ -4,3 +4,14 @@ with open("data.txt", "r") as file:
     print(file.read())
 
 print("kebab :3")
+
+moni = 0
+order = input("what pizza would u like to order S, M or L: ")
+
+if order == "S":
+    order1 = print("would you like a extra cheese? Y, N:")
+    if order1 == "Y":
+        print(f"total is {moni} enjoy! ")
+        
+else:
+    print("bye")
